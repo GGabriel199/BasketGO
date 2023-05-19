14d2a0c6aed5757eb155384605ec2fe5029e9b09
GGabriel199 <gabrielflorencio@outlook.com>
2023-05-19 20:37:00 -0300

    Basic game assets.

---
+++ Assets/SceneFunctions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFunctions : MonoBehaviour
+{
+    public void ChangeSceneByAddingIndex(int indexToAdd)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + indexToAdd);
+    }
+}
